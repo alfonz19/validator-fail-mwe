39bd79a19c26dbae01f80c65d42c57a897639926
Martin Mucha <martin.mucha@embedit.cz>
2023-05-22 14:43:03 +0200

    add forgotten controller ommitted by global .gitignore
    Signed-off-by: Martin Mucha <martin.mucha@embedit.cz>

---
+++ validator-fail-app/src/main/java/mwe/validatorfail/controller/TestController.java
@@ -0,0 +1,31 @@
+package mwe.validatorfail.controller;
+
+import mwe.validatorfail.dto.AnnotatedSampleDTO;
+import mwe.validatorfail.dto.SampleDTO;
+
+import javax.validation.Valid;
+
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = {"/test"})
+@Validated
+public class TestController {
+
+
+    @PostMapping(path = "annotated")
+    public String annotated(@RequestBody @Valid AnnotatedSampleDTO request) {
+        return "OK";
+    }
+
+    @PostMapping(path = "xml")
+    public String xml(@RequestBody @Valid SampleDTO request) {
+        return "OK";
+    }
+
+
+}
